2a5a00d207c3e97b55c76868b06b476585455c28
Hajime WAKAHARA <hajime.wakahara@gmail.com>
2013-02-12 10:03:36 +0900

    fix stuff forgot to repair

--- lib/juman/morpheme.rb
+++ lib/juman/morpheme.rb
@@ -4,13 +4,13 @@ class Juman
       attrs = line.split(/\s/)
       @surface, @pronunciation, @base, @pos = attrs.shift(4)
       @pos_id = attrs.shift.to_i
-      @pos_spec = attr_normalize(attrs.shift)
+      @pos_spec = normalize_attr(attrs.shift)
       @pos_spec_id = attrs.shift.to_i
-      @type = attr_normalize(attrs.shift)
+      @type = normalize_attr(attrs.shift)
       @type_id = attrs.shift.to_i
-      @form = attr_normalize(attrs.shift)
+      @form = normalize_attr(attrs.shift)
       @form_id = attrs.shift.to_i
-      @info = info_normalize(attrs.join(' '))
+      @info = normalize_info(attrs.join(' '))
     end
     attr_reader :surface, :pronunciation, :base, :pos, :pos_id, :pos_spec,
       :pos_spec_id, :type, :type_id, :form, :form_id, :info
